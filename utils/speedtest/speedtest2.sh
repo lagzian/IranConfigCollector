2168e0a4dcc284d6a69dfe9beccde2a9e37797e4
lagzian <134182864+lagzian@users.noreply.github.com>
2023-08-12 00:18:42 +0330

    Update speedtest2.sh

--- utils/speedtest/speedtest2.sh
+++ utils/speedtest/speedtest2.sh
@@ -2,5 +2,5 @@
 wget -O lite-linux-amd64 https://github.com/mahdibland/V2RayAggregator/releases/download/1.0.0/lite-linux-amd64-12
 wget -O lite_config.json https://raw.githubusercontent.com/lagzian/IranConfigCollector/main/utils/speedtest/lite_config.json
 #运行 LiteSpeedTest
-chmod +x ./lite-linux-amd64-12
-sudo nohup ./lite-linux-amd64-12 --config ./lite_config.json --test subs >speedtest.log 2>&1 &
+chmod +x ./lite-linux-amd64
+sudo nohup ./lite-linux-amd64 --config ./lite_config.json --test subs >speedtest.log 2>&1 &
