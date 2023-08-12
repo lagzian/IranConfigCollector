108f98e90e648f1c1e205732d08a9d7ca6cf5cb3
lagzian <134182864+lagzian@users.noreply.github.com>
2023-08-12 11:05:36 +0330

    Update main.go

--- main.go
+++ main.go
@@ -1,254 +1,94 @@
-package main
+name: Combined Workflow
 
-import (
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"os"
-	"regexp"
-	"strconv"
-	"strings"
+on:
+  schedule:
+    - cron: '0 */4 * * *'
+  workflow_dispatch:
 
-	"github.com/PuerkitoBio/goquery"
-)
+jobs:
+  sync_repository:
+    runs-on: ubuntu-latest
 
-var client = &http.Client{}
+    steps:
+      - name: Checkout repository
+        uses: actions/checkout@v2
 
-func main() {
+      - name: Install dependencies
+        run: pip install python-telegram-bot
 
-	channels := []string{
-		"https://t.me/s/v2rayng_fa2",
-		"https://t.me/s/v2rayng_org",
-		"https://t.me/s/V2rayNGvpni",
-		"https://t.me/s/custom_14",
-		"https://t.me/s/v2rayNG_VPNN",
-		"https://t.me/s/v2ray_outlineir",
-		"https://t.me/s/v2_vmess",
-		"https://t.me/s/FreeVlessVpn",
-		"https://t.me/s/freeland8",
-		"https://t.me/s/vmess_vless_v2rayng",
-		"https://t.me/s/PrivateVPNs",
-		"https://t.me/s/vmessiran",
-		"https://t.me/s/Outline_Vpn",
-		"https://t.me/s/vmessq",
-		"https://t.me/s/WeePeeN",
-		"https://t.me/s/V2rayNG3",
-		"https://t.me/s/ShadowsocksM",
-		"https://t.me/s/shadowsocksshop",
-		"https://t.me/s/v2rayan",
-		"https://t.me/s/ShadowSocks_s",
-		"https://t.me/s/VmessProtocol",
-		"https://t.me/s/napsternetv_config",
-		"https://t.me/s/Easy_Free_VPN",
-		"https://t.me/s/V2Ray_FreedomIran",
-		"https://t.me/s/V2RAY_VMESS_free",
-		"https://t.me/s/v2ray_for_free",
-		"https://t.me/s/V2rayN_Free",
-		"https://t.me/s/free4allVPN",
-		"https://t.me/s/vpn_ocean",
-		"https://t.me/s/configV2rayForFree",
-		"https://t.me/s/FreeV2rays{all_messages}",
-		"https://t.me/s/DigiV2ray",
-		"https://t.me/s/v2rayNG_VPN",
-		"https://t.me/s/freev2rayssr",
-		"https://t.me/s/v2rayn_server",
-		"https://t.me/s/Shadowlinkserverr",
-		"https://t.me/s/iranvpnet",
-		"https://t.me/s/vmess_iran",
-		"https://t.me/s/mahsaamoon1",
-		"https://t.me/s/V2RAY_NEW",
-		"https://t.me/s/v2RayChannel",
-		"https://t.me/s/configV2rayNG{all_messages}",
-		"https://t.me/s/config_v2ray",
-		"https://t.me/s/vpn_proxy_custom",
-		"https://t.me/s/vpnmasi{all_messages}",
-		"https://t.me/s/v2ray_custom",
-		"https://t.me/s/VPNCUSTOMIZE",
-		"https://t.me/s/HTTPCustomLand",
-		"https://t.me/s/vpn_proxy_custom",
-		"https://t.me/s/ViPVpn_v2ray",
-		"https://t.me/s/FreeNet1500",
-		"https://t.me/s/v2ray_ar{all_messages}",
-		"https://t.me/s/beta_v2ray",
-		"https://t.me/s/vip_vpn_2022",
-		"https://t.me/s/FOX_VPN66",
-		"https://t.me/s/VorTexIRN",
-		"https://t.me/s/YtTe3la",
-		"https://t.me/s/V2RayOxygen",
-		"https://t.me/s/Network_442",
-		"https://t.me/s/VPN_443",
-		"https://t.me/s/v2rayng_v",
-		"https://t.me/s/ultrasurf_12",
-		"https://t.me/s/iSeqaro{all_messages}",
-		"https://t.me/s/frev2rayng",
-		"https://t.me/s/frev2ray",
-		"https://t.me/s/FreakConfig",
-		"https://t.me/s/Awlix_ir",
-		"https://t.me/s/v2rayngvpn",
-		"https://t.me/s/God_CONFIG{all_messages}",
-		"https://t.me/s/Configforvpn01",
-	}
+      - name: Sync repository
+        env:
+          GH_TOKEN: ${{ secrets.GH_TOKEN1 }}
+        id: sync
+        run: |
+          gh repo sync lagzian/TelegramV2rayCollector -b main --force || echo "::set-output name=exit_code::1"
 
-	configs := map[string]string{
-		"ss":     "",
-		"vmess":  "",
-		"trojan": "",
-		"vless":  "",
-		"mixed":  "",
-	}
+      - name: Send Telegram notification on success
+        if: steps.sync.outputs.exit_code == '0'
+        env:
+          TELEGRAM_BOT_TOKEN: ${{ secrets.TELEGRAM_BOT_TOKEN }}
+          TELEGRAM_CHAT_ID: ${{ secrets.TELEGRAM_CHAT_ID }}
+        run: |
+          python - <<EOF
+          import asyncio
+          from telegram import Bot
 
-	myregex := map[string]string{
-		"ss":     `(.{3})ss:\/\/`,
-		"vmess":  `vmess:\/\/`,
-		"trojan": `trojan:\/\/`,
-		"vless":  `vless:\/\/`,
-	}
+          async def send_telegram_message():
+              bot_token = '${{ env.TELEGRAM_BOT_TOKEN }}'
+              bot = Bot(token=bot_token)
+              chat_id = '${{ env.TELEGRAM_CHAT_ID }}'
+              message = '✅✅🔥 **V2RAYFIXER** V2ray Repository Updated Successfully! 🔥✅✅'
+              await bot.send_message(chat_id=chat_id, text=message)
 
-	for i := 0; i < len(channels); i++ {
+          asyncio.run(send_telegram_message())
+          EOF
 
-		doc := loadChannel(channels[i])
+      - name: Send Telegram notification on failure
+        if: steps.sync.outputs.exit_code != '0'
+        env:
+          TELEGRAM_BOT_TOKEN: ${{ secrets.TELEGRAM_BOT_TOKEN }}
+          TELEGRAM_CHAT_ID: ${{ secrets.TELEGRAM_CHAT_ID }}
+        run: |
+          python - <<EOF
+          import asyncio
+          from telegram import Bot
 
-		doc.Find("code,pre").Slice(0, 10).Each(func(j int, s *goquery.Selection) {
-			message_text := s.Text()
-			lines := strings.Split(message_text, "\n")
-			for a := 0; a < len(lines); a++ {
-				for _, regex_value := range myregex {
-					re := regexp.MustCompile(regex_value)
-					lines[a] = re.ReplaceAllStringFunc(lines[a], func(match string) string {
-						return "\n" + match
-					})
-				}
-				for proto, _ := range configs {
-					if strings.Contains(lines[a], proto) {
-						configs["mixed"] += "\n" + lines[a] + "\n"
-					}
-				}
-			}
-		})
+          async def send_telegram_message():
+              bot_token = '${{ env.TELEGRAM_BOT_TOKEN }}'
+              bot = Bot(token=bot_token)
+              chat_id = '${{ env.TELEGRAM_CHAT_ID }}'
+              message = '❗❗⚠️⚠️🔥**V2RAYFIXER** V2ray Repository Sync Failed! 🔥⚠️⚠️❗❗'
+              await bot.send_message(chat_id=chat_id, text=message)
 
-	}
+          asyncio.run(send_telegram_message())
+          EOF
 
-	for proto, configcontent := range configs {
-		WriteToFile(RemoveDuplicate(configcontent), proto+"_iran.txt")
-	}
+  edit_readme:
+    needs: sync_repository  # Ensure that the sync_repository job is completed before starting this job
+    runs-on: ubuntu-latest
 
-}
+    steps:
+      - name: Checkout repository
+        uses: actions/checkout@v2
+        with:
+          repository: lagzian/TelegramV2rayCollector
+          path: repo  # Clone the repository to the "repo" directory
+          persist-credentials: false
+          fetch-depth: 0
 
-func loadChannel(channel string) *goquery.Document {
+      - name: Replace text in README.md
+        run: |
+          sed -i 's/yebekhe/lagzian/g' repo/README.md
+          cd repo
+          git config user.name "lagzian"
+          git config user.email "milad.lagzian@gmail.com"
+          git add README.md
 
-	req, _ := http.NewRequest("GET", channel, nil)
-	resp, _ := client.Do(req)
-
-	return goquery.NewDocumentFromReader(resp.Body)
-
-}
-
-func WriteToFile(fileContent string, filePath string) {
-
-	// Check if the file exists
-	if _, err := os.Stat(filePath); err == nil {
-		// If the file exists, clear its content
-		err = ioutil.WriteFile(filePath, []byte{}, 0644)
-		if err != nil {
-			fmt.Println("Error clearing file:", err)
-			return
-		}
-	} else if os.IsNotExist(err) {
-		// If the file does not exist, create it
-		_, err = os.Create(filePath)
-		if err != nil {
-			fmt.Println("Error creating file:", err)
-			return
-		}
-	} else {
-		// If there was some other error, print it and return
-		fmt.Println("Error checking file:", err)
-		return
-	}
-
-	// Write the new content to the file
-	err := ioutil.WriteFile(filePath, []byte(fileContent), 0644)
-	if err != nil {
-		fmt.Println("Error writing file:", err)
-		return
-	}
-
-	fmt.Println("File written successfully")
-}
-
-func load_more(link string) *goquery.Document {
-	req, _ := http.NewRequest("GET", link, nil)
-	fmt.Println(link)
-	resp, _ := client.Do(req)
-	doc, _ := goquery.NewDocumentFromReader(resp.Body)
-	return doc
-}
-
-func GetMessages(length int, doc *goquery.Document, number string, channel string) *goquery.Document {
-	x := load_more(channel + "?before=" + number)
-
-	html2, _ := x.Html()
-	reader2 := strings.NewReader(html2)
-	doc2, _ := goquery.NewDocumentFromReader(reader2)
-
-	// _, exist := doc.Find(".js-messages_more").Attr("href")
-	doc.Find("body").AppendSelection(doc2.Find("body").Children())
-
-	newDoc := goquery.NewDocumentFromNode(doc.Selection.Nodes[0])
-	// fmt.Println(newDoc.Find(".js-messages_more").Attr("href"))
-	messages := newDoc.Find(".js-widget_message_wrap").Length()
-
-	fmt.Println(messages)
-	if messages > length {
-		return newDoc
-	} else {
-		num, _ := strconv.Atoi(number)
-		n := num - 21
-		if n > 0 {
-			ns := strconv.Itoa(n)
-			GetMessages(length, newDoc, ns, channel)
-		} else {
-			return newDoc
-		}
-	}
-
-	return newDoc
-}
-
-func reverse(lines []string) []string {
-	for i := 0; i < len(lines)/2; i++ {
-		j := len(lines) - i - 1
-		lines[i], lines[j] = lines[j], lines[i]
-	}
-	return lines
-}
-
-func RemoveDuplicate(config string) string {
-	lines := strings.Split(config, "\n")
-
-	// Use a map to keep track of unique lines
-	uniqueLines := make(map[string]bool)
-
-	// Loop over lines and add unique lines to map
-	for _, line := range lines {
-		if len(line) > 0 {
-			uniqueLines[line] = true
-		}
-	}
-
-	// Join unique lines into a string
-	uniqueString := strings.Join(getKeys(uniqueLines), "\n")
-
-	return uniqueString
-}
-
-func getKeys(m map[string]bool) []string {
-	keys := make([]string, len(m))
-	i := 0
-	for k := range m {
-		keys[i] = k
-		i++
-	}
-	return keys
-}
+          # Check if there are any changes to commit
+          if git diff-index --quiet HEAD; then
+            echo "No changes to commit."
+          else
+            git commit -m "Replace 'yebekhe' with 'lagzian'"
+            git remote set-url origin https://${{ secrets.ACCESS_TOKEN }}@github.com/lagzian/TelegramV2rayCollector.git
+            git push origin HEAD:main
+          fi
